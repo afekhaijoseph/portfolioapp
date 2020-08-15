52fe3f598463035d52091e45dc11ac150796f1d9
afekhaijoseph <jobenzystein@gmail.com>
2020-08-15 12:05:40 +0100

    update settings

--- portfolioapp/settings.py
+++ portfolioapp/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfuhsdfhijdfuj783j34ih33h45jhjkjk3nj4k*(U&*GUY^7776gjhh'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-portfolio-creator.herokuapp.com'
+]
 
 
 # Application definition
